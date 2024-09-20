550d99df08fffe4665d66838ca0415cdb1471f8d
Dazzal David <91616790+dazzal-david@users.noreply.github.com>
2024-09-20 22:24:46 +0530

    Add files via upload
    Updated with New style and Error handling

--- scripts.js
+++ scripts.js
@@ -1,24 +1,34 @@
 const btn = document.getElementById('btn');
+const welcomeMsg = document.getElementById('welcomeMsg');
 
-btn.addEventListener('click',fetchData);
+btn.addEventListener('click', fetchData);
 
 async function fetchData() {
     try {
-
+        welcomeMsg.textContent = '';
         const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
         const resultText = document.getElementById('errorMsg');
         if (!response.ok) {
-            resultText.textContent = 'The Pokemon Was not Found'
+            resultText.textContent = 'The Pokémon Was not Found';
+            return;
         }
         const data = await response.json();
+        
+        // Displaying the Pokémon image
         const img = data.sprites.front_default;
         const resultImg = document.getElementById('resultImg');
-        resultText.style.display = "none"
         resultImg.src = img;
         resultImg.style.display = "block";
+
+        // Displaying additional Pokémon details
+        document.getElementById('pokemonHeight').textContent = `Height: ${data.height}`;
+        document.getElementById('pokemonWeight').textContent = `Weight: ${data.weight}`;
+        document.getElementById('pokemonType').textContent = `Type: ${data.types.map(type => type.type.name).join(', ')}`;
+        document.getElementById('pokemonAbilities').textContent = `Abilities: ${data.abilities.map(ability => ability.ability.name).join(', ')}`;
+
+        resultText.style.display = "none";
     } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+        errorMsg.textContent = 'No PokeMon Found';
     }
 }
-
